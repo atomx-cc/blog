dbaa07a485baa1cfae9f7faaf0e1de3b947e2859
Atomx3 <atom.x.hooray@gmail.com>
2025-03-05 10:40:25 +0100

    Update content

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,20 @@ git add .
 git commit -m "Update content" || true
 git push origin "$CURRENT_BRANCH"
 
-# 如果 public 已经是 worktree，则直接复用；否则创建 worktree
-if git worktree list | grep -q " public$"; then
-    echo "Worktree public already exists, reusing it."
-else
-    git worktree add -B gh-pages public origin/gh-pages
-fi
+# # 如果 public 已经是 worktree，则直接复用；否则创建 worktree
+# if git worktree list | grep -q " public$"; then
+#     echo "Worktree public already exists, reusing it."
+# else
+#     git worktree add -B gh-pages public origin/gh-pages
+# fi
+
+
+# 移除已存在的 public 工作树（忽略错误）
+git worktree remove -f public 2>/dev/null || true
+
+# 添加新的 gh-pages 工作树
+git worktree add -B gh-pages public origin/gh-pages
+
 
 # # Save CNAME file
 # cp public/CNAME CNAME.tmp || true
